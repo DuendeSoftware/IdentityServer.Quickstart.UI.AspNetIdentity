d5f9d8101e2e499edd6154e3a3547e82bdf0bd80
Brock Allen <brockallen@gmail.com>
2021-12-30 10:41:22 -0500

    update for v6.0

---
+++ getmain.ps1
@@ -0,0 +1,14 @@
+$source = "https://github.com/DuendeSoftware/IdentityServer.Quickstart.UI.AspNetIdentity/archive/main.zip"
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+Invoke-WebRequest $source -OutFile ui.zip
+
+Expand-Archive ui.zip
+
+if (!(Test-Path -Path Pages))       { mkdir Pages }
+if (!(Test-Path -Path wwwroot))     { mkdir wwwroot }
+
+copy .\ui\IdentityServer.Quickstart.UI-main\Pages\* Pages -recurse -force
+copy .\ui\IdentityServer.Quickstart.UI-main\wwwroot\* wwwroot -recurse -force
+
+del ui.zip
+del ui -recurse
---
+++ getmain.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -e
+
+SOURCE="https://github.com/DuendeSoftware/IdentityServer.Quickstart.UI.AspNetIdentity/archive/main.zip"
+curl -L -o ui.zip "$SOURCE"
+
+unzip -d ui ui.zip
+
+[[ -d Pages ]] || mkdir Pages
+[[ -d wwwroot ]] || mkdir wwwroot
+
+cp -r ./ui/IdentityServer.Quickstart.UI-main/Pages/* Pages
+cp -r ./ui/IdentityServer.Quickstart.UI-main/wwwroot/* wwwroot
+
+rm -rf ui ui.zip
